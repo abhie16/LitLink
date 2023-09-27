a15cbfe5116afc4761697d3c155a7099055e75b9
Abhishek pandey <abhie9336@gmail.com>
2023-09-27 19:20:11 +0530

    protected the routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Register from './components/Register';
 import { Toaster } from 'react-hot-toast';
 import Homepage from './components/Homepage';
 import { useSelector } from 'react-redux';
+import ProtectedRoute from './utils/ProtectedRoute';
+import PublicRoute from './utils/PublicRoute';
 
 function App() {
   const {loading} = useSelector(state => state.alerts);
@@ -12,15 +14,18 @@ function App() {
       <Router>
         {loading && 
           <div className='spinner-parent'>
-              <div class="spinner-border" role="status"></div>
+              <div className="spinner-border" role="status"></div>
           </div>
         }
         
         <Toaster position='top-center' reverseOrder={false} />
         <Routes>
-          <Route path='/login' element = {<Login />} />
-          <Route path='/register' element = {<Register />} />
-          <Route path='/' element = {<Homepage />} />
+          <Route path='/login' element = {<PublicRoute><Login /></PublicRoute>} />
+          <Route path='/register' element = {<PublicRoute><Register /></PublicRoute>} />
+          <Route path='/' element = {
+          <ProtectedRoute>
+            <Homepage />
+          </ProtectedRoute>} />
         </Routes>
       </Router>
   );
