93a9f87dddea896dc9dcee913f5ce4f10e327dad
Abhishek pandey <abhie9336@gmail.com>
2023-09-27 11:43:44 +0530

    completed register endpoint in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './components/Login';
 import Register from './components/Register';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div>
       <Router>
+        <Toaster position='top-center' reverseOrder={false} />
         <Routes>
           <Route path='/login' element = {<Login />} />
           <Route path='/register' element = {<Register />} />
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Form, Input, Button } from 'antd'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 const Register = () => {
 
-  const onFinish = (values) => {
-    console.log('Success:', values)
+  const navigate = useNavigate();
+  const onFinish = async(values) => {
+    
+    try {
+      const response = await axios.post('/api/user/register', values);
+      if(response.data.success){
+        toast.success(response.data.message);
+        toast("Redirecting to Login Page");
+        navigate('/login');
+      }
+      else{
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
   }
 
   return (
