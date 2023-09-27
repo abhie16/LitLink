bb03cee47bd3aaf4540f616578df87cfbc328d4c
Abhishek pandey <abhie9336@gmail.com>
2023-09-27 17:10:59 +0530

    implement middleware for protected endpoints

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const Homepage = () => {
+
+  const getData = () =>{
+    try {
+      const response = axios.post("/api/user/get-user-info-by-id", {},
+      {
+        headers:{
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>Homepage</div>
   )
