29f05c7910d7a378cffa68d12c2c9b8a92d5ab84
Abhishek pandey <abhie9336@gmail.com>
2023-09-27 10:47:55 +0530

    implemented register route

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
+app.use(express.json());
 
 const dbConfig = require('./config/dbConfig');
 
+const userRoute = require('./routes/userRoutes');
+
+
+app.use('/api/user', userRoute);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>{
