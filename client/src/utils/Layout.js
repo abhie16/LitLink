65dac5f28d82899023469948a1cc2059bd645e77
Abhishek pandey <abhie9336@gmail.com>
2023-09-28 14:12:07 +0530

    created sidebar

--- client/src/utils/Layout.js
+++ client/src/utils/Layout.js
@@ -1,16 +1,58 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import './Layout.css'
 
 function Layout({children}) {
+
+    const location = useLocation();
+
+    const userMenu = [
+        {
+            name: 'Home',
+            path: '/',
+            icon: 'ri-home-line'
+        },
+        {
+            name: 'Books',
+            path: '/books',
+            icon: 'ri-booklet-line'
+        },
+        {
+            name: 'Authors',
+            path: '/authors',
+            icon: 'ri-user-line'
+        },
+        {
+            name: 'Logout',
+            path: '/logout',
+            icon: 'ri-logout-box-line'
+        }
+    ]
+
+    const menuToBeRendered = userMenu;
+
   return (
     <div className='main'>
         <div className='d-flex layout'>
             <div className='sidebar'>
-                sidebar
+                <div className='sidebar-header'>
+                    <h1>LitLink</h1>
+                </div>
+                <div className='menu'>
+                    {menuToBeRendered.map((menu) =>{
+                        const isActive = location.pathname === menu.path;
+                        return(
+                            <div className={`d-flex menu-item ${isActive && 'active-menu-item'}`}>
+                                <i className={menu.icon}></i>
+                                <Link to={menu.path}>{menu.name}</Link>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
             <div className='content'>
                 <div className='header'>
-                    header
+                    <div className='d-flex flex-row-reverse'><i className="ri-notification-2-line"></i></div>
                 </div>
                 <div className='body'>
                     {children}
